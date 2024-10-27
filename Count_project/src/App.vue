71bde28f955d0bb504165ab6639919972be99587
brad-roman <143841269+brad-roman@users.noreply.github.com>
2024-10-27 21:53:46 +0000

    complete Counter project

---
+++ Count_project/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import counter from './components/counter.vue'
+</script>
+
+<template>
+  <header>
+      <div class="wrapper">
+      <counter/>
+    </div>
+  </header>
+
+  
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+</style>
