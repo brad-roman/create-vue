126034a58c52005ac745ab39a8b1b783e5c26ed9
brad-roman <143841269+brad-roman@users.noreply.github.com>
2025-01-20 04:21:18 +0000

    moved files

---
+++ table-project/src/components/playerAvail.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+    const props = defineProps({
+        avails: {type: Array,
+        default: []
+        }
+    })
+</script>
+
+<template>
+    <td v-for="(status, index) in props.avails" :key="index">
+        <span v-if="status === 'Y'">✅</span>
+        <span v-else-if="status === 'N'">❌</span>
+        <span v-else="status === 'U'">❓</span>
+    </td>
+</template>
